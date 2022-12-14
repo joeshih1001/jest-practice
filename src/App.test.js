72c6a0a0ceff867e4e18aad632307b2f18f94912
josh <josh@admindadteach.onmicrosoft.com>
2022-12-15 00:26:23 +0800

    button checkbox test

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,48 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, getByRole, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial color and update with click", () => {
-  render(<App/>)
-
+  render(<App />);
 
   //find an element with a role of button and text of 'Change to blue'
-  const colorButton = screen.getByRole('button', {name:'Change to blue'})
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
   //expect the background color to be red
-  expect(colorButton).toHaveStyle({backgroundColor:'red'})
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 
   //click button
-  fireEvent.click(colorButton)
+  fireEvent.click(colorButton);
 
   //expect the background color to be blue
-  expect(colorButton).toHaveStyle({backgroundColor:'blue'})
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
 
   //expect the button text to be 'Change to red'
-  expect(colorButton).toHaveTextContent('Change to red')
+  expect(colorButton).toHaveTextContent("Change to red");
 });
 
+test("initial condition", () => {
+  render(<App />);
+  //check that the button starts out enabled
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeEnabled();
+
+  //check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test('checkbox disables button on first click and enables on second click', () => {
+  render(<App />)
+  const checkbox = screen.getByRole('checkbox')
+  const button = screen.getByRole('button')
+ //test disabled
+ fireEvent.click(checkbox)
+ expect(checkbox).toBeChecked()
+ expect(button).toBeDisabled()
+
+ //test enabled
+ fireEvent.click(checkbox)
+ expect(checkbox).not.toBeChecked()
+ expect(button).toBeEnabled()
+
+})
